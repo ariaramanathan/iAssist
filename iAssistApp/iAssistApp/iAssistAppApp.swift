903d950c8a537e5a6c4402cf1befdf84eca73ee2
Priya Dalal-Whelan <priyadw00@gmail.com>
2025-03-10 16:28:41 -0400

    empty app creation

---
+++ iAssistApp/iAssistApp/iAssistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAssistAppApp.swift
+//  iAssistApp
+//
+//  Created by Priya Dalal-Whelan on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iAssistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
